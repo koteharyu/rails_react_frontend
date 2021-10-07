bfb256084f2acc84149feb7fda733fd566f44dd8
haryu <hryau6@gmail.com>
2021-10-07 16:36:03 +0900

    add: 新規投稿機能

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,7 +1,45 @@
-import { memo, VFC } from "react";
+import { Heading, FormControl, FormLabel, Input, Box, Button, Stack } from "@chakra-ui/react";
+import { ChangeEvent, memo, useState, VFC, } from "react";
+import { useHistory } from "react-router-dom";
+import axios from 'axios'
 
 export const NewPost: VFC = memo(() => {
+
+  const [newPostTitle, setNewPostTitle] = useState("")
+  const [newPostContent, setNewPostContent] = useState("")
+
+  const history = useHistory()
+
+  const createNewPost = () => {
+    const data = { title: newPostTitle, content: newPostContent}
+    axios.post('http://localhost:3001/posts', data)
+    .then(res => {
+      console.log(res.data)
+      history.push("/")
+    })
+    .catch(e => console.error(e))
+  }
+
+  const handlePostTitle = (e: ChangeEvent<HTMLInputElement>) => setNewPostTitle(e.target.value)
+  const handlePostContent = (e: ChangeEvent<HTMLInputElement>) => setNewPostContent(e.target.value)
+
+
   return (
-    <p>posts/new</p>
+    <>
+      <Heading as="h1" color="teal" textAlign="center">NEW POST</Heading>
+      <Box ml={6}>
+        <Stack>
+          <FormControl>
+            <FormLabel>Title</FormLabel>
+            <Input type="text" placeholder="some content" value={newPostTitle} onChange={handlePostTitle} />
+          </FormControl>
+          <FormLabel>Title</FormLabel>
+          <FormControl>
+            <Input type="text" placeholder="some content" value={newPostContent} onChange={handlePostContent} />
+            <Button onClick={createNewPost}>Create</Button>
+          </FormControl>
+        </Stack>
+      </Box>
+    </>
   )
 })
