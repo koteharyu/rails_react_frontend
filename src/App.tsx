da3f379314f837917ed68af3a323919062fca6bd
haryu <hryau6@gmail.com>
2021-10-07 15:42:59 +0900

    add: 投稿一覧の取得に成功

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter } from 'react-router-dom'
 import './App.css';
 import { Router } from './router/Router';
--- src/components/AllPosts.tsx
+++ src/components/AllPosts.tsx
@@ -1,7 +1,37 @@
-import { memo, VFC } from "react";
+import { memo, useEffect, useState, VFC } from "react";
+import axios from 'axios'
+
+type Post = {
+  id: number
+  title: string
+  content: string
+}
 
 export const AllPosts: VFC = memo(() => {
+
+const [posts, setPosts] = useState<Array<Post> | null>(null)
+
+const getAllPosts = () => {
+  axios.get('http://localhost:3001/posts')
+    .then(res => {
+      setPosts(res.data)
+    })
+    .catch((e) => console.error(e))
+}
+
+useEffect(() => {
+  getAllPosts()
+}, [posts])
+
   return (
-    <p>posts/index</p>
+    <>
+      <p>Posts Index</p>
+      {posts?.map((post) => (
+        <div key={post.id}>
+          <p>{post.title}</p>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </>
   )
 })
