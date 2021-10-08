5b19dc77bfed90244362b3b6f39b294ba06689bc
haryu <hryau6@gmail.com>
2021-10-08 11:04:13 +0900

    add: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import './App.css';
+import { Header } from './components/Header';
 import { Router } from './router/Router';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Router />
       </BrowserRouter>
     </>
